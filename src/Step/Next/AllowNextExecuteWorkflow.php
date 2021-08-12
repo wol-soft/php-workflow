d94fff2f55fa9a2e060f5456ec33042bcc55d155
Enno Woortmann <enno.woortmann@web.de>
2021-08-12 12:21:41 +0200

    Add workflow control

--- src/Step/Next/AllowNextExecuteWorkflow.php
+++ src/Step/Next/AllowNextExecuteWorkflow.php
@@ -5,21 +5,23 @@ declare(strict_types=1);
 namespace PHPWorkflow\Step\Next;
 
 use Exception;
-use PHPWorkflow\Exception\WorkflowControl\FailWorkflowException;
-use PHPWorkflow\Exception\WorkflowControl\SkipWorkflowException;
 use PHPWorkflow\State\WorkflowState;
 use PHPWorkflow\WorkflowControl;
 
 trait AllowNextExecuteWorkflow
 {
-    public function executeWorkflow(): void
+    public function executeWorkflow(bool $throwOnFailure = true): bool
     {
         try {
             $this->workflow->run(new WorkflowState(new WorkflowControl()));
-        } catch (SkipWorkflowException | FailWorkflowException $exception) {
-
         } catch (Exception $exception) {
+            if ($throwOnFailure) {
+                throw $exception;
+            }
 
+            return false;
         }
+
+        return true;
     }
 }
